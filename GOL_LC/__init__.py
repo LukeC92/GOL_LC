496fe249d3f1d524a12253f88e4e01ca4694ac1c
Carroll, Luke <luke.carroll@metoffice.gov.uk>
2017-07-14 16:57:19 +0100

    Completed GOL Implementation. Code may need some tidying up and removing of unnnecesary remnants, but functionally this program is complete.

--- GOL_LC/__init__.py
+++ GOL_LC/__init__.py
@@ -16,6 +16,13 @@ from PIL import Image
 np.set_printoptions(threshold=np.inf)
 
 
+full = np.ones((5,5))
+
+full_update = np.zeros((5,5))
+full_update[0,0]=1
+full_update[4,0]=1
+full_update[0,4]=1
+full_update[4,4]=1
 
 #defining a small array on which the blinker oscillator is plotted
 blinker = np.zeros((5,5))
@@ -70,86 +77,6 @@ def sumcells(x, y, A):
         elif(x==A.shape[0]-1):
             return A[x-1,y-1]+A[x-1,y]+A[x,y-1]
         
-        
-        
-full = np.ones((7,7))
-aa0 = np.arange(25).reshape(5, 5)
-nx = aa0.shape[0]
-ny = aa0.shape[1]
-#print(nx)
-#print(ny)
-#aa0P = np.pad(aa0,((1,1),(1,1)), mode='constant')
-#aa1 = np.roll(np.roll(aa0, 1, axis=0), 1, axis=1)
-#aa1P = aa0P[0:nx, :ny]
-#aa2 = np.roll(aa0, 1, axis=0)
-#aa2P = aa0P[0:nx,1:ny+1]
-#aa3 = np.roll(np.roll(aa0, 1, axis=0), -1, axis=-1)
-#aa3P = aa0P[0:nx,2:ny+3]
-#aa4 = np.roll(aa0, 1, axis=1)
-#aa4P = aa0P[1:nx+1,0:ny]
-#aa5 = np.roll(aa0, -1, axis=1)
-#aa5P = aa0P[1:nx+1, 2:ny+2]
-#aa6 = np.roll(np.roll(aa0, -1, axis=0), 1, axis=1)
-#aa6P = aa0P[2:nx+2, 0:ny]
-#aa7 = np.roll(aa0, -1, axis=0)
-#aa7P = aa0P[2:nx+2, 1:ny+1]
-#aa8 = np.roll(np.roll(aa0, -1, axis=0), -1, axis=1)
-#aa8P = aa0P[2:nx+2, 2:ny+2]
-#aa1P1 = np.pad(aa0,((0,0),(1,0)), mode='constant')
-#aa1P2 = np.pad(aa0,((0,0),(1,0)), mode='constant')[:, :-1]
-#aa1P3 = np.pad(aa0,((1,0),(1,0)), mode='constant')[:-1, :-1]
-
-#
-#x = np.array([[1, 2, 3],[4, 5, 6]])
-#
-#print np.pad(x,((0,0),(1,0)), mode='constant')[:, :-1]
-
-empty = np.zeros(aa0.shape)
-#aa1E = aa1*empty[0:5, 0]
-##
-#print("0")
-#print(aa0)
-#print("0")
-#print(aa0P)
-#print("1")
-#print(aa1)
-#print("1")
-#print(aa1P)
-#print("2")
-#print(aa2)
-#print("2")
-#print(aa2P)
-#print("3")
-#print(aa3)
-#print("3")
-#print(aa3P)
-#print("4")
-#print(aa4)
-#print("4")
-#print(aa4P)
-#print("5")
-#print(aa5)
-#print("5")
-#print(aa5P)
-#print("6")
-#print(aa6)
-#print("6")
-#print(aa6P)
-#print("7")
-#print(aa7)
-#print("7")
-#print(aa7P)
-#print("8")
-#print(aa8)
-#print("8")
-#print(aa8P)
-#print(empty)
-#print(aa1E)
-#print(aa1C)
-#print(aa1P1)
-#print(aa1P2)
-#print(aa1P3)
-        
 def neighbours(A):
     """
     This function returns an array showing the number of neighbours
@@ -162,7 +89,6 @@ def neighbours(A):
     return : np.array
         The number of neighbours for each cell
     """
-
     A0 = np.pad(A,((1,1),(1,1)), mode='constant')
     A1 = A0[0:-2, 0:-2]
     A2 = A0[0:-2, 1:-1]
@@ -171,46 +97,8 @@ def neighbours(A):
     A5 = A0[1:-1, 2:]
     A6 = A0[2:, 0:-2]
     A7 = A0[2:, 1:-1]
-    A8 = A0[2:, 2:]
-    
-#    A1 = A0[0:nxc, 0:nyc]
-#    A2 = A0[0:nxc, 1:nyc+1]
-#    A3 = A0[0:nxc, 2:nyc+2]
-#    A4 = A0[1:nxc+1, 0:nyc]
-#    A5 = A0[1:nxc+1, 2:nyc+2]
-#    A6 = A0[2:nxc+2, 0:nyc]
-#    A7 = A0[2:nxc+2, 1:nyc+1]
-#    A8 = A0[2:nxc+2, 2:nyc+2]
-    
-
-#    A1 = np.roll(np.roll(A, 1, axis=0), 1, axis=1)
-#    A1 = np.pad(A1,((1,0),(1,0)), mode='constant')[:-1, :-1]
-#    A1[:,0]=0
-#    A1[0,:]=0
-#    A2 = np.roll(A, 1, axis=0)
-#    A2[0,:]=0
-#    A3 = np.roll(np.roll(A, 1, axis=0), -1, axis=-1)
-#    A3[0,:]=0
-#    A4 = np.roll(A, 1, axis=1)
-#    A5 = np.roll(A, -1, axis=1)
-#    A6 = np.roll(np.roll(A, -1, axis=0), 1, axis=1)
-#    A7 = np.roll(A, -1, axis=0)
-#    A8 = np.roll(np.roll(A, -1, axis=0), -1, axis=1)
-    
+    A8 = A0[2:, 2:]  
     return A1+A2+A3+A4+A5+A6+A7+A8
-
-print(neighbours(full))
-print(neighbours(blinker))
-#
-#truty = (neighbours(blinker).any() >2)
-#print(truty)
-
-def testfunct(x):
-    if(x.any()>2): return True
-    else:    return False
-
-
-#print(testfunct(neighbours(blinker)))
 
 def alive(x, y, A):
     """
@@ -233,7 +121,47 @@ def alive(x, y, A):
     v = A[x,y]
     t = sumcells(x,y,A)
     return ((v==1 and t in [2,3]) or (v==0 and t==3))
+
+
+
+DC = blinker == 1
+X = neighbours(blinker)
+Y = X == 2
+Z = X == 3
+LL = np.logical_or(Y, Z)
+KK = np.logical_and(LL, DC)
+BD = blinker==0
+BDL = np.logical_and(BD, Z)
+BF = np.logical_or(BDL, KK)
+
+print(X)
+print(BF)
+
+
+def updateM(A):
+    """
+    This function takes an array and returns the state it will be in
+    in the next generation. It does this using array logic.
     
+    Parameters
+    ----------
+    A : np.array
+        The array to be updated
+    return : np.array
+        The state of the array in the next generation.
+    """
+    live = A==1
+    neighbourS = neighbours(A)
+    neighbour2 = neighbourS == 2
+    neighbour3 = neighbourS == 3
+    neighbour2or3 = np.logical_or(neighbour2, neighbour3)
+    live2live = np.logical_and(neighbour2or3, live)
+    dead = A==0
+    dead2live = np.logical_and(dead, neighbour3)
+    nowLiving = np.logical_or(dead2live, live2live)
+    result = nowLiving.astype(np.uint8)
+    return result
+   
    
 def updatecell(x,y,A):
     """
@@ -356,6 +284,25 @@ if __name__ == '__main__':
         return graph,
     
     
+    def animateM(B):
+        """
+        This function updates a given array B and graph's input to it, 
+        thus animating from one generation to the next, this uses
+        updateM which uses array logic.
+        
+        Parameters
+        ----------
+        B : np.array
+            The GOL board to be updated
+        return : The updated state of the GOL board
+        """
+        B[:] = updateM(B)
+        C = B.flatten()
+        graph.set_array(C)
+
+        return graph,    
+    
+    
     #The user thought out this was unnecessary, turns out it is necessary.
     def init():
         """
@@ -370,4 +317,12 @@ if __name__ == '__main__':
     #parameter and in graph above
     #ani = animation.FuncAnimation(fig, animate, [c0], init_func=init, 
     #                              interval=100, blit=False)
+    
+    
+    #animates a game of life with the start state listed as the 3rd uses updateM
+    #parameter and in graph above
+    aniM = animation.FuncAnimation(fig, animateM, [c0], 
+                                   init_func=init, interval=100, blit=False)
+    
+    
     #plt.show()
--- GOL_LC/tests/test_gol.py
+++ GOL_LC/tests/test_gol.py
@@ -75,12 +75,78 @@ class TestNeighbours(unittest.TestCase):
     def setUp(self):
         makearrays(self)
         self.blinkerN = np.zeros((5,5))
-        self.blinkerN[1,2] = 1
-        self.blinkerN[2,2] = 1
-        self.blinkerN[3,2] = 1
+        self.blinkerN[0,1:4] = 1
+        self.blinkerN[4,1:4] = 1
+        self.blinkerN[1,1]=2
+        self.blinkerN[1,3]=2
+        self.blinkerN[2,2]=2
+        self.blinkerN[3,1]=2
+        self.blinkerN[3,3]=2
+        self.blinkerN[2,1]=3
+        self.blinkerN[2,3]=3
+        self.blinkerN[1,2]=1
+        self.blinkerN[3,2]=1
+        
+        self.blinkerUN = np.array([[ 0,  0,  0,  0,  0,],
+                                 [ 1,  2,  3,  2,  1,],
+                                 [ 1,  1,  2,  1,  1,],
+                                 [ 1,  2,  3,  2,  1,],
+                                 [ 0,  0,  0,  0,  0,]])
+        
+        
+        self.fullN = np.array([[ 3,  5,  5,  5,  3,],
+                               [ 5,  8,  8,  8,  5,],
+                               [ 5,  8,  8,  8,  5,],
+                               [ 5,  8,  8,  8,  5,],
+                               [ 3,  5,  5,  5,  3,]])
+        
+        self.fullUN = np.array([[ 0,  1,  0,  1,  0,],
+                                [ 1,  1,  0,  1,  1,],
+                                [ 0,  0,  0,  0,  0,],
+                                [ 1,  1,  0,  1,  1,],
+                                [ 0,  1,  0,  1,  0,]])
         
     def test_blinker_neighbour(self):
         result = GOL_LC.neighbours(self.blinker)
+        assert_array_equal(result, self.blinkerN)
+        
+        
+    def test_blinker_update_neighbour(self):
+        result = GOL_LC.neighbours(self.blinker_update)
+        assert_array_equal(result, self.blinkerUN)    
+        
+    def test_full_neighbour(self):
+        result = GOL_LC.neighbours(self.full)
+        assert_array_equal(result, self.fullN)   
+        
+    def test_full_update_neighbour(self):
+        result = GOL_LC.neighbours(self.full_update)
+        assert_array_equal(result, self.fullUN)              
+
+
+class TestUpdateM(unittest.TestCase):
+    def setUp(self):
+        makearrays(self)
+        
+    def test_blinker_updateM(self):
+        result = GOL_LC.updateM(self.blinker)
+        assert_array_equal(result, self.blinker_update)
+        
+    def test_blinker_updateM_2(self):
+        result = GOL_LC.updateM(GOL_LC.updateM(self.blinker))
+        assert_array_equal(result, self.blinker)
+
+    def test_full_updateM(self):
+        result = GOL_LC.updateM(self.full)
+        assert_array_equal(result, self.full_update)
+
+    def test_full_updateM_2(self):
+        result = GOL_LC.updateM(GOL_LC.updateM(self.full))
+        assert_array_equal(result, self.dead)
+        
+    def test_dead_updateM(self):
+        result = GOL_LC.updateM(self.dead)
+        assert_array_equal(result, self.dead)
 
 class TestUpdate(unittest.TestCase):  
 
